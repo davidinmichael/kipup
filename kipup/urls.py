8dd21a232adc0deb29685e01ddb3f0284ffa8a69
davidinmichael <davidinmichael@gmail.com>
2025-04-17 16:31:45 +0100

    Feature: Sign up screen added.

--- kipup/urls.py
+++ kipup/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
     path("", include("core.urls")),
+    path("account/", include("account.urls")),
     # path("account/", include("account.urls")),
 ]
 
