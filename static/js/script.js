f76208f8146528fd09cf41c6a27ea683598e82d6
davidinmichael <davidinmichael@gmail.com>
2025-04-23 13:24:55 +0100

    Feature: Registration now working wth frontend.

--- static/js/script.js
+++ static/js/script.js
@@ -10,4 +10,63 @@ window.addEventListener("DOMContentLoaded", () => {
 		const navLinks = document.getElementById("navLinks");
 		navLinks.classList.toggle("toggle-nav");
 	});
+
+	let submitBtn = document.getElementById("regSubmitBtn");
+	let passwordError = document.getElementById("passwordError");
+	let emailError = document.getElementById("emailError");
+	let agree = document.getElementById("agree");
+
+	agree.addEventListener("click", () => {
+		if (!agree.checked) {
+			submitBtn.disabled = true;
+		} else {
+			emailError.textContent = "";
+		}
+	});
+
+	
+	let regForm = document.getElementById("registerForm");
+	regForm.addEventListener("submit", async (e) => {
+		e.preventDefault();
+		let email = e.target.email.value;
+		let password = e.target.password.value;
+		let confirmPassword = e.target.confirm_password.value;
+		
+		let formValues = {
+			first_name: e.target.first_name.value,
+			last_name: e.target.last_name.value,
+			email: e.target.email.value,
+			gender: e.target.gender.value,
+			password: e.target.password.value
+		}
+		
+		if (email === "") {
+			setTimeout(() => {
+				emailError.textContent = "Error in email";
+			}, 1000);
+		} else if (email.includes("admin")) {
+			emailError.textContent = "Email cannot contain admin keyword";
+
+		} else if (password !== confirmPassword) {
+			passwordError.textContent = "Passwords do not match."
+		} else {
+			try {
+				const response = await fetch("/account/api/register/", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(formValues)
+				});
+				console.log(response);
+				if (response.ok) {
+					alert("User created successfully, continue to login.");
+					window.location.href = "/account/login/";
+				}
+			} catch {
+				console.log("Error");
+			}
+		}
+
+	});
 });
